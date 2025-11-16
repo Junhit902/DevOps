f84f6c1a92b3707a6555fa47cef62cdfb995d298
Thiago Jun Honma <112987953+Junhit902@users.noreply.github.com>
2025-11-16 16:41:45 -0300

    Create Aluno entity following DDD and Clean Architecture principles

---
+++ src/main/java/com/projeto/meuprojeto/domain/entities/Aluno.java
@@ -0,0 +1,48 @@
+package com.projeto.meuprojeto.domain.entities;
+
+import java.util.Objects;
+
+public class Aluno {
+    private final String id;
+    private String name;
+    private String email;
+
+    public Aluno(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno)) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(id, aluno.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
