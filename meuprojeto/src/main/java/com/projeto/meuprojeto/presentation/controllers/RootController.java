6e14dbfcdc0ad59480cfd84126022f59bbb81a5a
Thiago Jun <thiagojunhonma@gmail.com>
2025-11-17 17:35:34 -0300

    root controller adicionado

---
+++ meuprojeto/src/main/java/com/projeto/meuprojeto/presentation/controllers/RootController.java
@@ -0,0 +1,13 @@
+package com.projeto.meuprojeto.presentation.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Aplicação está funcionando!";
+    }
+}
